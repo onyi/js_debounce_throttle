6966b7961786a840bc53f5bbc66cafffa810684f
Onyi Cho <onyicho@gmail.com>
2019-08-22 23:49:53 -0700

    Add throttle demonstration

--- index.js
+++ index.js
@@ -4,34 +4,58 @@ window.addEventListener("DOMContentLoaded", () => {
     let debButton = document.createElement("button");
     debButton.innerHTML = "Click me for Debounce"
 
-    const generatePsudoText = () => {
+    const generatePsudoText = (text) => {
         let span = document.createElement("span");
-        span.innerHTML = `<br> ${new Date().toUTCString()} - Helloworld <br>`;
+        span.innerHTML = `<br> ${new Date().toUTCString()} - ${text || "Helloworld"} <br>`;
         return span;
     }
 
     debButton.addEventListener("click", debounce(1000, function() {
-        console.log('I am clicked!');
-        root.appendChild(generatePsudoText())
+        console.log('Debounce button clicked!');
+        root.appendChild(generatePsudoText(`Debounce action`))
     }));
 
     root.appendChild(debButton);
+
+
+
+
+    let thButton = document.createElement("button");
+    thButton.innerHTML = `Click me for Throttle!`;
+    thButton.addEventListener("click", throttle(2000, function(){
+        console.log(`Throttle button clicked!`);   
+        root.appendChild(generatePsudoText("Throttle action"));
+    }))
+
+    root.appendChild(thButton);
+
 })
 
 const debounce = (delay, callback) => {
 
     let debTimeout;
-    console.log(`Debounce`)
     return function(){
         const context = this;
         const args = arguments;
         clearTimeout(debTimeout);
         debTimeout = setTimeout(
             () => {
-                console.log(`Hi?`)
                 callback.apply(context, args)
             }
         , delay);
+    }
+}
+
+const throttle = (limit, callback) => {
+    let isThrottling;
+    return function(){
+        let context = this;
+        let args = arguments;
+        if(!isThrottling){
+            callback.apply(context, args);
+            isThrottling = true;
+            setTimeout( () => isThrottling = false, limit);
+        }
     }
 }
 
